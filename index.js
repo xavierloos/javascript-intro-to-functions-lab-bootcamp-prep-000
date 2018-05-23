30f27e6da69b6011d419b3853928e947fa82d5de
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-05-23 20:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
+<<<<<<< HEAD
 if (string == whisper(string)){
     return "I can't hear you!"
   }
@@ -21,3 +22,22 @@ if (string == whisper(string)){
   if (string == "I love you, Grandma."){
     return "I love you, too."
   }
+=======
+function sayHiToGrandma(string) {
+  var lowercase = "hello"
+  if (lowercase.toLowerCase() === lowercase) {
+  return "I can\'t hear you!"
+  }
+  var uppercase = "HELLO!"
+  if (uppercase.toUpperCase() === uppercase) {
+  return "YES INDEED!"
+  }
+}
+function sayHiToGrandma(string) {
+ 
+  var uppercase = "HELLO!"
+  if (uppercase.toUpperCase() === uppercase) {
+  return "YES INDEED!"
+  }
+}
+>>>>>>> ac40883a5aa13036a8c2b52d46a6c78d4b722755
